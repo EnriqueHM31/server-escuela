4b181a5db3d2324863436f438762c22aa8ef648d
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-16 16:29:21 -0600

    agregar rutas para eliminar y buscar usuarios en el servidor

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import express from "express";
 import { clerkMiddleware } from "@clerk/express";
 import cambioContraseña from "./routes/cambiarContraseña/correo";
 import modificarContraseña from "./routes/cambiarContraseña/modificar";
-import registrarUsuario from "./routes/usuarios/registro/registro";
+import registrarUsuario from "./routes/usuarios/registro/registroJefes";
+import eliminarUsuario from "./routes/usuarios/eliminar/eliminarJefes";
+import busquedaUsuariosAll from "./routes/usuarios/busqueda/busquedaAll";
 
 const app = express();
 
@@ -22,6 +24,8 @@ console.log("CLERK_SECRET_KEY:", process.env.CLERK_SECRET_KEY); // DEBUG
 app.use("/cambiarcontrasena", cambioContraseña);
 app.use("/modificarcontrasena", modificarContraseña);
 app.use("/registro", registrarUsuario);
+app.use("/eliminar", eliminarUsuario);
+app.use("/busqueda", busquedaUsuariosAll);
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
